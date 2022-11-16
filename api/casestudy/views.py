aa967af560a7a09cdd349a79094921500c2ac540
Cristian Blanco <crismablanco@gmail.com>
2022-11-16 00:01:17 -0300

    REFACTOR for data validation

--- api/casestudy/views.py
+++ api/casestudy/views.py
@@ -6,7 +6,7 @@ from .services import NumbersService
 class NumberConverterToWordsView(APIView):
     def get_number_in_words(self, number: int, and_word: str = "") -> str:
         service = NumbersService(and_word=and_word)
-        num_in_english = service.convert(number=number)
+        num_in_english = service.convert(number=int(number) if number.isdigit() else -1)
         status_code = 200 if num_in_english else 500
         status_str = "ok" if num_in_english else "fail"
         return Response(
@@ -14,13 +14,13 @@ class NumberConverterToWordsView(APIView):
         )
 
     def post(self, request):
-        requested_number = int(request.data.get("number", -1))
+        requested_number = request.data.get("number", "")
         and_word = request.data.get("and_word", "")
 
         return self.get_number_in_words(number=requested_number, and_word=and_word)
 
     def get(self, request):
-        requested_number = int(request.GET.get("number", -1))
+        requested_number = request.GET.get("number", "")
         and_word = request.GET.get("and_word", "")
 
         return self.get_number_in_words(number=requested_number, and_word=and_word)
